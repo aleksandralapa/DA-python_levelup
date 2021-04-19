141ef8ab8fd8688a5c525dfafc69a2c8f1f06069
aleksandralapa <56296552+aleksandralapa@users.noreply.github.com>
2021-04-19 22:32:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, status
 
 app = FastAPI()
 
 @app.get("/")
-def root():
-    return {"message": "Hello world!"}
+def root_view():
+    return {"message": "Hello World"}
+
+methods = {"GET", "DELETE", "PUT", "OPTIONS"}
 
 @app.get("/method/{method}")
 def method_check(method: str, response: Response):
     if method in methods:
-        return f"message: {method}"
-    else:
+        return {"method": f"{method}"}
+    elif method == "POST":
         response.status_code = status.HTTP_201_CREATED
-    return f"message: {method}"
+    return {"method": f"{method}"}
