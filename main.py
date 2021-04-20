af219aa951cb44303df5a668f50ea99ba3bd1e67
aleksandralapa <56296552+aleksandralapa@users.noreply.github.com>
2021-04-20 22:27:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 
 app = FastAPI()
 app.counter = 0
-app.patients = [0]
+app.patients = dict()
 
 class new_patient(BaseModel):
     name: str
@@ -19,13 +19,13 @@ class old_patient(BaseModel):
     register_date: str
     vaccination_date: str
 
-#zad1.1
+#zad1
 @app.get("/")
 def root_view():
     return {"message": "Hello world!"}
 
 
-#zad1.2
+#zad2
 @app.get("/method")
 def GET():
     return {"method": "GET"}
@@ -46,7 +46,7 @@ def OPTIONS():
 def POST():
     return {"method": "POST"}
 
-#zad1.3
+#zad3
 @app.get('/auth', status_code=204)
 def password_check(response: Response, password: Optional[str] = None, password_hash: Optional[str] = None):
     if (not password) or (not password_hash):
@@ -58,7 +58,7 @@ def password_check(response: Response, password: Optional[str] = None, password_
         response.status_code = 401
         return
 
-#zad1.4
+#zad4
 @app.post('/register', status_code=201)
 def registration (new: new_patient):
     app.counter+=1
@@ -69,15 +69,14 @@ def registration (new: new_patient):
     for i in len:
         if i.isalpha():
             x=x+1
-    date2 = datetime.today() + timedelta(days=x)
+    date2 = datetime.today() + timedelta(days = x)
     date2 = date2.strftime('%Y-%m-%d')
     result = old_patient (id = app.counter, name = new.name, surname = new.surname, register_date = date, vaccination_date = date2)
-    app.patients.append(result)
+    app.patients[app.counter] = result
     return result
-
-#zad1.5
+#zad5
 @app.get('/patient/{id}', status_code=200)
-def list_patient (response: Response, id: int):
+def list_patent (response: Response, id: int):
     if id < 1:
         response.status_code = 400
         return
@@ -85,5 +84,6 @@ def list_patient (response: Response, id: int):
         response.status_code = 404
         return
     return app.patients[id]
+
 
 
