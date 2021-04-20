08e996b26b2419f348a96082abbd3c07687b065f
aleksandralapa <56296552+aleksandralapa@users.noreply.github.com>
2021-04-20 18:38:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+from hashlib import sha512
+from typing import Optional
 
 app = FastAPI()
 
@@ -29,3 +31,12 @@ def OPTIONS():
 @app.post("/method", status_code = 201)
 def POST():
     return {"method": "POST"}
+
+#zad3
+@app.get('/auth', status_code=204)
+def password_check(response: Response, password: Optional[str]=None,password_hash: Optional[str]=None):
+    password = password.encode(encoding = 'UTF-8', errors = 'ignore')
+    password = sha512(password.strip()).hexdigest()
+    if password != password_hash or password is None or password_hash is None:
+        response.status_code = 401
+        return
