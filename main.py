317f8e74e7337b5a230091da3c9c63fb5cbb66c7
aleksandralapa <56296552+aleksandralapa@users.noreply.github.com>
2021-05-04 23:05:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -133,9 +133,9 @@ def token_authorization(response: Response, login_data: HTTPBasicCredentials = D
 
 
 #zad3.3
+#zad3.3
 @app.get("/welcome_session")
-def swelcome(response: Response, format: Optional[str] = None):
-    session_token = Cookie(None)
+def swelcome(response: Response, format: Optional[str] = None, session_token: str = Cookie(None)):
     if session_token != app.login_session:
         response.status_code = 401
         return
@@ -147,8 +147,8 @@ def swelcome(response: Response, format: Optional[str] = None):
         return PlainTextResponse("Welcome!")
 
 @app.get("/welcome_token")
-def twelcome(response: Response, token: Optional[str] = None, format: Optional[str] = None,):
-    if (token is None) or (token != app.login_token):
+def twelcome(response: Response, token: str = '', format: str = ''):
+    if (token == '') or (token != app.login_token):
         response.status_code = 401
         return
     if format == "json":
